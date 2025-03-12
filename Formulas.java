d3f44c6ab783c0b1551d6bf0212d318e498e4d6b
Tako <168281472+tkts-tako@users.noreply.github.com>
2025-03-12 14:05:01 +0000

    push

--- Formulas.java
+++ Formulas.java
@@ -136,7 +136,7 @@ public class Formulas
                                 System.out.println("     ---");
                                 System.out.println("     " + R4);
                                 System.out.println();
-                                System.out.println("I1 = " + I4 + "A");
+                                System.out.println("I4 = " + I4 + "A");
                                 break;
 
                             case '5':
@@ -160,12 +160,266 @@ public class Formulas
                                 System.out.println();
                                 System.out.println("IL = " + IL + "A");
                                 break;
+                                
 
                             default:
                                 System.out.println("Não foi selecionada nenhuma das opções.");
                             }
 
                             break;
+                    case '2':
+                        System.out.println();
+                        System.out.println();
+                        System.out.println();
+                        System.out.println("1. Calcular R1");
+                        System.out.println("2. Calcular R2");
+                        System.out.println("3. Calcular R3");
+                        System.out.println("4. Calcular R4");
+                        System.out.println("5. Calcular RL");
+
+                        iimput = sc.next().charAt(0);
+
+                        switch (iimput)
+                        {
+                            case '1':
+                                System.out.println();
+                                System.out.println();
+                                System.out.println();
+                                System.out.println("R1 = Ui");
+                                System.out.println("     --");
+                                System.out.println("     I1");
+                                System.out.println("");
+
+                                System.out.println("Ui = ");
+                                Ui = sc.nextDouble();
+
+                                System.out.println("I1 = ");
+                                I1 = sc.nextDouble();
+
+                                R1 = Ui / I1;
+
+                                System.out.println();
+                                System.out.println("R1 = " + Ui);
+                                System.out.println("     ---");
+                                System.out.println("     " + I1);
+                                System.out.println();
+                                System.out.println("R1 = " + R1 + "Ohm");
+                                break;
+
+                            case '2':
+                                System.out.println("R2 = Ui");
+                                System.out.println("     --");
+                                System.out.println("     I2");
+                                System.out.println("");
+
+                                System.out.println("Ui = ");
+                                Ui = sc.nextDouble();
+
+                                System.out.println("I2 = ");
+                                I2 = sc.nextDouble();
+
+                                R2 = Ui / I2;
+
+                                System.out.println();
+                                System.out.println("R2 = " + Ui);
+                                System.out.println("     ---");
+                                System.out.println("     " + I2);
+                                System.out.println();
+                                System.out.println("R2 = " + R2 + "Ohm");
+                                break;
+
+                            case '3':
+                                System.out.println("R3 = Ui");
+                                System.out.println("     --");
+                                System.out.println("     I3");
+                                System.out.println("");
+
+                                System.out.println("Ui = ");
+                                Ui = sc.nextDouble();
+
+                                System.out.println("I3 = ");
+                                I3 = sc.nextDouble();
+
+                                R3 = Ui / I3;
+
+                                System.out.println();
+                                System.out.println("R3 = " + Ui);
+                                System.out.println("     ---");
+                                System.out.println("     " + I3);
+                                System.out.println();
+                                System.out.println("R3 = " + R3 + "Ohm");
+                                break;
+
+                            case '4':
+                                System.out.println("R4 = Ui");
+                                System.out.println("     --");
+                                System.out.println("     I4");
+                                System.out.println("");
+
+                                System.out.println("Ui = ");
+                                Ui = sc.nextDouble();
+
+                                System.out.println("I4 = ");
+                                I4 = sc.nextDouble();
+
+                                R4 = Ui / I4;
+
+                                System.out.println();
+                                System.out.println("R4 = " + Ui);
+                                System.out.println("     ---");
+                                System.out.println("     " + I4);
+                                System.out.println();
+                                System.out.println("R4 = " + R4 + "Ohm");
+                                break;
+
+                            case '5':
+                                System.out.println("RL = Ui");
+                                System.out.println("     --");
+                                System.out.println("     IL");
+                                System.out.println("");
+
+                                System.out.println("Ui = ");
+                                Ui = sc.nextDouble();
+
+                                System.out.println("IL = ");
+                                IL = sc.nextDouble();
+
+                                RL = Ui / IL;
+                                
+                                System.out.println();
+                                System.out.println("RL = " + Ui);
+                                System.out.println("     ---");
+                                System.out.println("     " + IL);
+                                System.out.println();
+                                System.out.println("RL = " + RL + "Ohm");
+                                break;
+
+                            default:
+                                System.out.println("Não foi selecionada nenhuma das opções.");
+                        }
+
+                    case '3':
+                        System.out.println();
+                        System.out.println();
+                        System.out.println();
+                        System.out.println("1. Calcular Uo com R1");
+                        System.out.println("2. Calcular Uo com R2");
+                        System.out.println("3. Calcular Uo com R3");
+                        System.out.println("4. Calcular Uo com R4");
+                        System.out.println("5. Calcular Uo com RL");
+
+                        iimput = sc.next().charAt(0);
+
+                        switch (iimput)
+                        {
+                            case '1':
+                                System.out.println();
+                                System.out.println();
+                                System.out.println();
+                                System.out.println("Uo = R1 * I1");
+                                System.out.println("");
+
+                                System.out.println("R1 = ");
+                                R1 = sc.nextDouble();
+
+                                System.out.println("I1 = ");
+                                I1 = sc.nextDouble();
+
+                                Uo = R1 * I1;
+
+                                System.out.println();
+                                System.out.println("Uo = " + R1 + " * " + I1);
+                                System.out.println();
+                                System.out.println("Uo = " + Uo + "V");
+                                break;
+
+                            case '2':
+                                System.out.println();
+                                System.out.println();
+                                System.out.println();
+                                System.out.println("Uo = R2 * I2");
+                                System.out.println("");
+
+                                System.out.println("R2 = ");
+                                R2 = sc.nextDouble();
+
+                                System.out.println("I2 = ");
+                                I2 = sc.nextDouble();
+
+                                Uo = R2 * I2;
+
+                                System.out.println();
+                                System.out.println("Uo = " + R2 + " * " + I2);
+                                System.out.println();
+                                System.out.println("Uo = " + Uo + "V");
+                                break;
+
+                            case '3':
+                                System.out.println();
+                                System.out.println();
+                                System.out.println();
+                                System.out.println("Uo = R3 * I3");
+                                System.out.println("");
+
+                                System.out.println("R3 = ");
+                                R3 = sc.nextDouble();
+
+                                System.out.println("I3 = ");
+                                I3 = sc.nextDouble();
+
+                                Uo = R3 * I3;
+
+                                System.out.println();
+                                System.out.println("Uo = " + R3 + " * " + I3);
+                                System.out.println();
+                                System.out.println("Uo = " + Uo + "V");
+                                break;
+
+                            case '4':
+                                System.out.println();
+                                System.out.println();
+                                System.out.println();
+                                System.out.println("Uo = R4 * I4");
+                                System.out.println("");
+
+                                System.out.println("R4 = ");
+                                R4 = sc.nextDouble();
+
+                                System.out.println("I4 = ");
+                                I4 = sc.nextDouble();
+
+                                Uo = R4 * I4;
+
+                                System.out.println();
+                                System.out.println("Uo = " + R4 + " * " + I4);
+                                System.out.println();
+                                System.out.println("Uo = " + Uo + "V");
+                                break;
+
+                            case '5':
+                                System.out.println();
+                                System.out.println();
+                                System.out.println();
+                                System.out.println("Uo = RL * IL");
+                                System.out.println("");
+
+                                System.out.println("RL = ");
+                                RL = sc.nextDouble();
+
+                                System.out.println("IL = ");
+                                IL = sc.nextDouble();
+
+                                Uo = RL * IL;
+
+                                System.out.println();
+                                System.out.println("Uo = " + RL + " * " + IL);
+                                System.out.println();
+                                System.out.println("Uo = " + Uo + "V");
+                                break;
+
+                            default:
+                                System.out.println("Não foi selecionada nenhuma das opções.");
+                        }
 
                     default:
                         System.out.println("Não foi selecionada nenhuma das opções.");
